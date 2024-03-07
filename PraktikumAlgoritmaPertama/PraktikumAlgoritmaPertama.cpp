d821d1418694aba04adfdc8534f4b1c7dd5efd81
ZakyMalika <zakymalika0987@gmail.com>
2024-03-07 19:00:36 +0700

    menambahkan data ke

--- PraktikumAlgoritmaPertama/PraktikumAlgoritmaPertama.cpp
+++ PraktikumAlgoritmaPertama/PraktikumAlgoritmaPertama.cpp
@@ -19,4 +19,10 @@ void input() {	//prosedur untuk input
 	cout << "=========================" << endl;
 	cout << "Masukan Elemen Array" << endl;
 	cout << "=========================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ":";
+		cin >> a[i];
+	}
+
 }
